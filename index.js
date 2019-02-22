9095b0134c272b13bcbf4ff86d537733d2da82bc
smiling-ping-3937 <shawn.wilson137@gmail.com>
2019-02-22 21:39:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,32 +4,3 @@ var b = 10
 function add(){
   return a + b
 }
-
-
-
-/*
-function sub(a, b){
-  a -= b
-}
-
-sub()
-
-function mul(a, b){
-  return a * b
-}
-
-mul()
-
-function div(a, b){
-  return a / b
-}
-
-div()
-
-function inc(n){
-  return n++
-}
-
-function dec(n){
-  return n--
-}*/
